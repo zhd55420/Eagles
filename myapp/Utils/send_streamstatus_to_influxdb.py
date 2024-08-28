b63c5f26e14971c8a22a2b088037d05ab806f70f
zhd55420 <554207375@qq.com>
2024-08-28 10:04:04 +0800

    增加功能：调整send_streamstatus代码逻辑

--- myapp/Utils/send_streamstatus_to_influxdb.py
+++ myapp/Utils/send_streamstatus_to_influxdb.py
@@ -1,18 +1,14 @@
-import os
-import subprocess
-import logging
 import datetime
 import json
-from influxdb import InfluxDBClient
 import requests
-from pytz import UTC
+from influxdb import InfluxDBClient
+import logging
 
-logger = logging.getLogger('streamstatus')
+logger = logging.getLogger(__name__)
 
-def all_stream_task():
+
+def fetch_and_process_stream_data(api_url, measurement_name):
     try:
-        # 这里放置 All_stream.py 的逻辑代码
-        bk_url = "http://3.15.111.189:2082/api/rapidStreamStatus/query/v1"
         HEADERS = {
             "Content-Type": "application/json ;charset=utf-8 "
         }
@@ -25,30 +21,17 @@ def all_stream_task():
             }
         }
         json_data = json.dumps(data_request)
-        res = requests.post(bk_url, data=json_data, headers=HEADERS)
+        res = requests.post(api_url, data=json_data, headers=HEADERS)
         now_time = datetime.datetime.utcnow()
         points = []
         for e in json.loads(res.text)['data']:
-            masterStreamStatus = "null"
-            transcodeStreamStatus = "null"
-            channelReceiverStreamStatus = "null"
-            bandWidth = 0
-            if e['streamStatus']['masterStreamStatus'] == None:
-                masterStreamStatus = False
-                bandWidth = 0
-            else:
-                masterStreamStatus = True
-                bandWidth = e['streamStatus']['masterStreamStatus']['bw']
-            if e['streamStatus']['transcodeStreamStatus'] == None:
-                transcodeStreamStatus = False
-            else:
-                transcodeStreamStatus = True
-            if e['streamStatus']['channelReceiverStreamStatus'] == None:
-                channelReceiverStreamStatus = False
-            else:
-                channelReceiverStreamStatus = True
+            masterStreamStatus = e['streamStatus']['masterStreamStatus'] is not None
+            transcodeStreamStatus = e['streamStatus']['transcodeStreamStatus'] is not None
+            channelReceiverStreamStatus = e['streamStatus']['channelReceiverStreamStatus'] is not None
+            bandWidth = e['streamStatus']['masterStreamStatus']['bw'] if masterStreamStatus else 0
+
             point = {
-                "measurement": "rapid_stream_status",
+                "measurement": measurement_name,
                 "tags": {
                     "stream_id": e['streamResponse']['streamId'],
                     "source": e['streamResponse']['source'],
@@ -66,78 +49,21 @@ def all_stream_task():
                 }
             }
             points.append(point)
-        print(points)
+
         client = InfluxDBClient(host='15.204.133.239', port=8086, database='rapid_stream', username='uploader',
                                 password='bja!d7BB')
-        client.write_points(points, database='rapid_stream')
+        client.write_points(points, database='rapid_stream',batch_size=1000)
         client.close()
-        logger.info("Running All Stream Task")
+        logger.info(f"Running Task for {measurement_name}")
     except Exception as e:
-        logger.error(f"Error in All Stream Task: {str(e)}")
+        logger.error(f"Error in Task for {measurement_name}: {str(e)}")
+
+
+def all_stream_task():
+    api_url = "http://3.15.111.189:2082/api/rapidStreamStatus/query/v1"
+    fetch_and_process_stream_data(api_url, "rapid_stream_status")
 
 
 def all_goose_stream_task():
-    try:
-        # 这里放置 All_goose_stream.py 的逻辑代码
-        bk_url = "http://54.237.33.107:2082/api/rapidStreamStatus/query/v1"
-        HEADERS = {
-            "Content-Type": "application/json ;charset=utf-8 "
-        }
-        data_request = {
-            "streamRequest": {
-                "source": ""
-            },
-            "gooseStreamRequest": {
-                "source": ""
-            }
-        }
-        json_data = json.dumps(data_request)
-        res = requests.post(bk_url, data=json_data, headers=HEADERS)
-        now_time = datetime.datetime.utcnow()
-        points = []
-        for e in json.loads(res.text)['data']:
-            masterStreamStatus = "null"
-            transcodeStreamStatus = "null"
-            channelReceiverStreamStatus = "null"
-            bandWidth = 0
-            if e['streamStatus']['masterStreamStatus'] == None:
-                masterStreamStatus = False
-                bandWidth = 0
-            else:
-                masterStreamStatus = True
-                bandWidth = e['streamStatus']['masterStreamStatus']['bw']
-            if e['streamStatus']['transcodeStreamStatus'] == None:
-                transcodeStreamStatus = False
-            else:
-                transcodeStreamStatus = True
-            if e['streamStatus']['channelReceiverStreamStatus'] == None:
-                channelReceiverStreamStatus = False
-            else:
-                channelReceiverStreamStatus = True
-            point = {
-                "measurement": "goose_rapid_stream_status",
-                "tags": {
-                    "stream_id": e['streamResponse']['streamId'],
-                    "source": e['streamResponse']['source'],
-                    "signalType": e['streamResponse']['signalType']
-                },
-                "time": now_time,
-                "fields": {
-                    "stream": e['streamResponse']['streamId'],
-                    "masterStatus": masterStreamStatus,
-                    "master_server_id": e['streamResponse']['masterServer']['serverId'],
-                    "transcodeStatus": transcodeStreamStatus,
-                    "forward_server_id": e['streamResponse']['forwardServer']['serverId'],
-                    "receiverStatus": channelReceiverStreamStatus,
-                    "bandWidth": bandWidth
-                }
-            }
-            points.append(point)
-        print(points)
-        client = InfluxDBClient(host='15.204.133.239', port=8086, database='rapid_stream', username='uploader',
-                                password='bja!d7BB')
-        client.write_points(points, database='rapid_stream')
-        client.close()
-        logger.info("Running All Goose Stream Task")
-    except Exception as e:
-        logger.error(f"Error in All Goose Stream Task: {str(e)}")
+    api_url = "http://54.237.33.107:2082/api/rapidStreamStatus/query/v1"
+    fetch_and_process_stream_data(api_url, "goose_rapid_stream_status")
